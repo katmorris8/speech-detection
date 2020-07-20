36201cc63098616b53dde37edb7c6245eb6ba27f
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-07-19 21:02:32 -0400

    created the elements for the speach to render into

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
